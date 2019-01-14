ce12b4973a256f012435416cc3aab3c1d8eeee85
keerthiRAV <44790028+keerthiRAV@users.noreply.github.com>
2019-01-14 16:52:57 -0500

    WEEK 2 CLASS HAND IN WORK

---
+++ script.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+//Name:Keerthi Raveendran
+//Date: January 14, 2019
+//student:C0742322
+//COURSE: CSD2214 SECTION 6
+
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = document.querySelector('.days');
+const hoursSpan = document.querySelector('.hours');
+const minutesSpan = document.querySelector('.minutes');
+const secondsSpan = document.querySelector('.seconds');
+
+
+//alert("yeah!!!! I am a great javascript programmer");
+console.log("hey keerthi");
+console.log(endDate.value);
